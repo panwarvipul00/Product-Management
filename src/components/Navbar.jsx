67cba3f54b4265235a6fdfc46d1cbdd8d0209acb
panwarvipul00 <panwarvipul1234@gmail.com>
2026-01-03 19:31:59 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ function Navbar() {
   return (
     <div className="w-full bg-white shadow-md px-6 py-4 flex justify-between">
       <h1 className="text-2xl font-bold tracking-wide bg-gradient-to-r from-indigo-600 to-pink-500 text-transparent bg-clip-text">
-        NovaTech
+        Grey Scientific Labs
       </h1>
       <span className="text-sm text-gray-500">
         Product Dashboard
